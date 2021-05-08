36ebc023eb040bc40e3478d6923f0ca5d0399baf
admodev <admosdigital@yahoo.com>
2021-05-08 13:46:05 -0300

    Agregar archivos de proyecto.

---
+++ GameEngine/GameEngine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace GameEngine.GameEngine
+{
+    class Canvas : Form
+    {
+        public Canvas()
+        {
+            this.DoubleBuffered = true;
+        }
+    }
+    public abstract class GameEngine
+    {
+        private Vector2 ScreenSize = new Vector2(512, 512);
+
+        private string Title;
+
+        public GameEngine(Vector2 ScreenSize, string Title)
+        {
+            this.ScreenSize = ScreenSize;
+            this.Title = Title;
+        }
+    }
+}
